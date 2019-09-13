42aa8d19aaf6fd022c7c845c462e8d6d11d272af
Kasper <Schonsteren@gmail.com>
2019-09-13 22:36:52 +0200

    - An attempt was made :) - Added classification and prediction

--- lecture_3/lecture_3_743.py
+++ lecture_3/lecture_3_743.py
@@ -5,65 +5,85 @@
 # Date : 11/9-2019
 # Lecture: 3  Parametric and nonparametric methods
 
-from scipy.io import loadmat
-from scipy.stats import norm
-from scipy.stats import multivariate_normal as mvn
-
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-from pprint import pprint
-mat_file = loadmat("dataset1_G_noisy.mat")
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+import scipy.io
 
-classes = (mat_file["trn_x"], mat_file["trn_y"])
+#Loads the mat file
+mat_file = scipy.io.loadmat('dataset1_G_noisy.mat')
 
-train_x_class = mat_file["trn_x_class"]
-train_y_class = mat_file["trn_y_class"]
+train_x = mat_file["trn_x"]
+train_y = mat_file["trn_y"]
 
-fig, ax = plt.subplots(1, 1)
+test_xy = mat_file["tst_xy"]
+test_xy_class = mat_file["tst_xy_class"]
+
+#Priors for class 1 and class 2
+prior_C1 = len(train_x)/(len(train_x)+len(train_y))
+prior_C2 = len(train_y)/(len(train_y)+len(train_x))
+print("Prior values for class1: {} and class2: {}".format(round(prior_C1,4),round(prior_C2,4)))
+
+#Mean Values
+mean_C1 = (np.mean(train_x[:,0]),np.mean(train_x[:,1]))
+mean_C2 = (np.mean(train_y[:,0]),np.mean(train_y[:,1]))
+
+#Standard Diviation
+std_C1 = (np.std(train_x[:,0]),np.std(train_x[:,1]))
+std_C2 = (np.std(train_y[:,0]),np.std(train_y[:,1]))
+
+#Variance
+var_C1 = np.var(train_y) #Not in use
+var_C2 = np.var(train_y) #Not in use
+
+#Covariance Matrix
+covM_C1 = np.cov(train_x[:,0],train_x[:,1])
+covM_C2 = np.cov(train_y[:,0],train_y[:,1])
+
+#Function to classify new data
+def classify(x):
+  #Posterior = likelihood*prior. MATH: P(x|c1)*P(c1)
+  p_C1 = (norm.pdf(x[0],mean_C1[0],std_C1[0])*
+          norm.pdf(x[1],mean_C1[1],std_C1[1])*
+          prior_C1)
+
+  p_C2 = (norm.pdf(x[0],mean_C2[0],std_C2[0])*
+          norm.pdf(x[1],mean_C2[1],std_C2[1])*
+          prior_C2)
+
+  #Returns the class value
+  if p_C1 > p_C2:
+    return 1
+  else:
+    return 2
+
+
 
 def task_a():
     """
     Classify instances in tst_xy, and use the corresponding label file tst_xy_class to calculate the accuracy
     """
-    
-    test_xy = mat_file["tst_xy"]
-    pprint(mat_file["trn_x"][0:])
-    #plt.scatter(mat_file["trn_x"][:,0],mat_file["trn_x"][:,1])
-    #plt.scatter(mat_file["trn_y"][:,0],mat_file["trn_y"][:,1])
-    
-    #Standard div, mean and variance of x:
-    stdx = np.std(mat_file["trn_x"])
-    meanx = np.mean(mat_file["trn_x"])
-    variancex = np.var(mat_file["trn_x"])
 
-    #Standard div, mean and variance of y:
-    stdy = np.std(mat_file["trn_y"])
-    meany = np.mean(mat_file["trn_y"])
-    variancey = np.var(mat_file["trn_y"])
+    predictions = pd.DataFrame(index=np.arange(0,len(test_xy)),columns=["Class"]) #Initialize empty dataframe that consists of the length of the test data
+    for index,data_row in enumerate(test_xy): #Goes through each row in test_xy
+        predictions.at[index,"Class"] = classify(data_row) #Classifies each row in test_xy and sets the returned class value to the dataframe of predictions
 
-    print(stdx)
-    print(meanx)
-    print(variancex)
-    print(stdy)
-    print(meany)
-    print(variancey)
-    
-    hest = np.array(mat_file["trn_x"]).T
+    correct_counter = 0 #Counts the number of times the classification is correct
+    for index,data_row in enumerate(test_xy): #Goes through each row in test_xy
+      if predictions.at[index,"Class"] == test_xy_class[index][0]: #Checks if the predicted classification is the same as the true classification
+        correct_counter += 1
 
-    cov = np.cov(hest)
-    print(cov)
-    print(mat_file["trn_x"].shape)
+    accuracy = correct_counter/len(test_xy) #Calculates the accuracy of how many correct classifications that have been done
+    print("Correct Predictions: {}. False Predictions: {}.".format(correct_counter,len(test_xy)-correct_counter))
+    print("The accuracy is: {}%".format(round(accuracy,4)))
 
-    print("Hest is:\t{}\ntrainx is:\t{}".format(hest.shape,mat_file["trn_x"].shape))
-
-    
-    #multi = mvn.pdf(test_xy,0,cov)
-    #print(multi)
-    #plt.show()
-    
-    #sns.scatterplot(x="x",y="y",data=sns.load_dataset(mat_file["trn_x"]))
-    
+    fig, ax = plt.subplots()
+    ax.scatter(train_x[:,0],train_x[:,1]) #Blue class 1
+    ax.scatter(train_y[:,0],train_y[:,1]) #Orange class 2
+    ax.scatter(mean_C1[0],mean_C1[1],c="red") #Mean for class 1
+    ax.scatter(mean_C2[0],mean_C2[1],c="red") #Mean for class 2
+    plt.show()
 
 def task_b():
     """
