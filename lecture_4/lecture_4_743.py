807238d6e624752e3d5e09eea0bc60f7a603a0c4
Kasper <Schonsteren@gmail.com>
2019-09-21 11:12:04 +0200

    - Now uses the test data instead

--- lecture_4/lecture_4_743.py
+++ lecture_4/lecture_4_743.py
@@ -39,14 +39,19 @@ def collect_labels(dataframe,mat_array,label): #dataframe = dataframe that conta
   return dataframe #Returns the dataset of labels
 
 mat_file = loadmat("mnist_all.mat") #Loading the mat file which includes the MNIST dataset
-labels_df = pd.DataFrame(columns=['Label']) #Declares pandas dataframe which contains labels
-labels_df = collect_labels(labels_df,mat_file['train5'],5) #Collects labels for each digit 5 image and saves it in the label dataframe
-labels_df = collect_labels(labels_df,mat_file['train6'],6) #Collects labels for each digit 6 image and saves it in the label dataframe
-labels_df = collect_labels(labels_df,mat_file['train8'],8) #Collects labels for each digit 8 image and saves it in the label dataframe
+train_labels_df = pd.DataFrame(columns=['Label']) #Declares pandas dataframe which contains train labels
+train_labels_df = collect_labels(train_labels_df,mat_file['train5'],5) #Collects labels for each digit 5 image and saves it in the train label dataframe
+train_labels_df = collect_labels(train_labels_df,mat_file['train6'],6) #Collects labels for each digit 6 image and saves it in the train label dataframe
+train_labels_df = collect_labels(train_labels_df,mat_file['train8'],8) #Collects labels for each digit 8 image and saves it in the train label dataframe
+
+test_labels_df = pd.DataFrame(columns=['Label']) #Declares pandas dataframe which contains test labels
+test_labels_df = collect_labels(test_labels_df,mat_file['test5'],5) #Collects labels for each digit 8 image and saves it in the test label dataframe
+test_labels_df = collect_labels(test_labels_df,mat_file['test6'],6) #Collects labels for each digit 8 image and saves it in the test label dataframe
+test_labels_df = collect_labels(test_labels_df,mat_file['test8'],8) #Collects labels for each digit 8 image and saves it in the test label dataframe
 
 #_______________SETUP DATASETS FOR TRAIN AND TEST__________________#
 train = np.concatenate((mat_file['train5'],mat_file['train6'],mat_file['train8'])) #Adds all three digits to one numpy array for the train dataset
-test = np.concatenate((mat_file['train5'],mat_file['train6'],mat_file['train8'])) #Adds all three digits to one numpy array for the test dataset
+test = np.concatenate((mat_file['test5'],mat_file['test6'],mat_file['test8'])) #Adds all three digits to one numpy array for the test dataset
 
 #________________________PRE-PROCESSING____________________________#
 print("[INFO]: Pre-Processing...")
@@ -65,7 +70,7 @@ test = pca.transform(test) #Applies dimentionality reduction to test dataset
 print("[INFO]: Classificationn...")
 #______________LOGISTIC REGRESSION (CLASSIFICATION)________________#
 logisticRegr = LogisticRegression(solver='lbfgs',multi_class='multinomial') #Used for classification
-logisticRegr.fit(train,labels_df['Label'].astype('int')) #Fits the logistic regression model
+logisticRegr.fit(train,train_labels_df['Label'].astype('int')) #Fits the logistic regression model
 predictions = logisticRegr.predict(test) #Predicts labels of the test dataset
 
 #__________________CALCULATE MODEL ACCURACY________________________#
@@ -78,14 +83,14 @@ def calculate_accuracy(predictions,labels_df): #Calculates the accuracy of the o
   accuracy = round(((correct_predictions/len(predictions))*100),3) #Calculates the accuracy of correct predictions compared to the number of predictions.
   print(f"[MODEL]: The model accuracy is: {accuracy}%") #Outputs the accuracy
 
-calculate_accuracy(predictions,labels_df) #Calls the function.
+calculate_accuracy(predictions,test_labels_df) #Calls the function.
 
 #_________________________SCATTER PLOT_____________________________#
 print("[INFO]: Plotting...")
 fig, ax = plt.subplots()
-index_5_begin = labels_df.loc[labels_df['Label']==5.0].index[0] #Locates the first index of the label 5 in the columns 'label' in the dataframe of labels. This enables the possibility of plotting the x,y positions of the dimentional reduced digits of 5
-index_6_begin = labels_df.loc[labels_df['Label']==6.0].index[0] #Locates the first index of the label 6 in the columns 'label' in the dataframe of labels. This enables the possibility of plotting the x,y positions of the dimentional reduced digits of 6
-index_8_begin = labels_df.loc[labels_df['Label']==8.0].index[0] #Locates the first index of the label 8 in the columns 'label' in the dataframe of labels. This enables the possibility of plotting the x,y positions of the dimentional reduced digits of 8
+index_5_begin = train_labels_df.loc[train_labels_df['Label']==5.0].index[0] #Locates the first index of the label 5 in the columns 'label' in the dataframe of labels. This enables the possibility of plotting the x,y positions of the dimentional reduced digits of 5
+index_6_begin = train_labels_df.loc[train_labels_df['Label']==6.0].index[0] #Locates the first index of the label 6 in the columns 'label' in the dataframe of labels. This enables the possibility of plotting the x,y positions of the dimentional reduced digits of 6
+index_8_begin = train_labels_df.loc[train_labels_df['Label']==8.0].index[0] #Locates the first index of the label 8 in the columns 'label' in the dataframe of labels. This enables the possibility of plotting the x,y positions of the dimentional reduced digits of 8
 
 #Digit 5
 ax.scatter(train[index_5_begin:index_6_begin,0],
