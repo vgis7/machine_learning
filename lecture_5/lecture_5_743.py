22c5142f72c451cfb054250be82665f1ffd20089
LynzZen <mathias.s.lynge@gmail.com>
2019-09-26 15:02:33 +0200

    Plottet the differenet means

--- lecture_5/lecture_5_743.py
+++ lecture_5/lecture_5_743.py
@@ -22,9 +22,31 @@ mat_file = loadmat("2D3classes.mat") #Loading the matfile with the 2d data for t
 
 X = np.concatenate((mat_file['trn5'],mat_file['trn6'],mat_file['trn8'])) #Adds all three digits to one numpy array for the train dataset
 
+trainSV5 =  mat_file['trn5']
+trainSV6 =  mat_file['trn6']
+trainSV8 =  mat_file['trn8']
+
+stdTrainSV5 = np.std(trainSV5)
+stdTrainSV6 = np.std(trainSV6)
+stdTrainSV8 = np.std(trainSV8)
+
+meanTrainSV5 = (np.mean(trainSV5[:,0]),np.mean(trainSV5[:,1]))
+meanTrainSV6 = (np.mean(trainSV6[:,0]),np.mean(trainSV6[:,1]))
+meanTrainSV8 = (np.mean(trainSV8[:,0]),np.mean(trainSV8[:,1]))
+
+
+print(stdTrainSV5)
+print(meanTrainSV8)
+
+
 kmean = KMeans(n_clusters=3)
 kmean.fit(X)
 
 plt.scatter(X[:,0],X[:,1])
-plt.scatter(kmean.cluster_centers_[:,0],kmean.cluster_centers_[:,1])
+plt.scatter(kmean.cluster_centers_[0,0],kmean.cluster_centers_[0,1],c="y",marker="$c1$")
+plt.scatter(kmean.cluster_centers_[1,0],kmean.cluster_centers_[1,1],c="y",marker="$c2$")
+plt.scatter(kmean.cluster_centers_[2,0],kmean.cluster_centers_[2,1],c="y",marker="$c3$")
+plt.scatter(meanTrainSV5[0],meanTrainSV5[1],c="r",marker="$5$")
+plt.scatter(meanTrainSV6[0],meanTrainSV6[1],c="r",marker="$6$")
+plt.scatter(meanTrainSV8[0],meanTrainSV8[1],c="r",marker="$8$")
 plt.show()
